602a8f89bb250193b3a278882cbee2dc128a24c7
JADHADI1 <Aditya.Jadhav@ksb.com>
2025-06-08 13:28:04 +0530

    Backend project added.

---
+++ Backend/DatingApp/API/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        //api/usrs/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if(user is null)
+                return NotFound();
+            return Ok(user);
+        }
+    }
+}
---
+++ Backend/DatingApp/API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace API.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
