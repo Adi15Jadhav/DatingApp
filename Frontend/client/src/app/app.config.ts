50101759be9f36053196f2f14929b17620db3bf4
JADHADI1 <Aditya.Jadhav@ksb.com>
2025-06-24 16:08:41 +0530

    Added routes

--- Frontend/client/src/app/app.config.ts
+++ Frontend/client/src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { positionElements } from 'ngx-bootstrap/positioning';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(),
     provideAnimations(),
+    provideToastr(),
   ],
 };
--- Frontend/client/src/app/nav/nav.component.ts
+++ Frontend/client/src/app/nav/nav.component.ts
@@ -3,22 +3,28 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, CommonModule, BsDropdownModule],
+  imports: [FormsModule, CommonModule, BsDropdownModule, RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  router = inject(Router);
+  toaster = inject(ToastrService);
+
   model: any = {};
   login() {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
+        this.router.navigateByUrl('/members');
         console.log(response);
       },
-      error: (error) => console.log(error),
+      error: (error) => this.toaster.error('Error occured'),
     });
   }
 
